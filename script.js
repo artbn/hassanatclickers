c891dbc4aee86d31240b5491d4ab61394b1eff33
artbn <ghaidastation@yahoo.com>
2014-07-11 22:06:59 -0400

    LST 9

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ var s5 = false
 var s6 = false
 var s7 = false
 var s8 = false
-var count = localStorage.getItem("hassanat count")
+//var count = localStorage.getItem("hassanat count")
 var hasanat = function() {
     if (first === 1) {
     alert("WARNING: NUMBERS ATTACHED TO CERTAIN DEEDS ARE NOT AT ALL REPRESENTATIVE OF THE ACTUAL REWARD ALLAH SWT GIVES! THESE NUMBERS ARE ARBITRARY! ALLAH SWT MULTIPLIES THE REWARD TO WHOM HE PLEASES!")
